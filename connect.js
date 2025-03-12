b3b7ae458c1ab41f7fd4b586affd7b1a8431039b
Irene Fabbri <irenefabbri.ed@gmail.com>
2025-03-12 16:06:40 +0100

    DB conncetion and error handling

--- connect.js
+++ connect.js
@@ -1,63 +1,55 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const DB = new sqlite3.Database('./exercises.db', sqlite3.OPEN_READWRITE, connected);
+function setupDB(next) {
+    const DB = new sqlite3.Database('./exercises.db', sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE, connected);
 
-function connected(err){
-    if(err) {
-        return res.status(500).send({
-            "errors": [
-                {
-                    "status": "500",
-                    "code": "db-creation-error",          
-                    "title": "DB creation error",
-                    "detail": "There have been issues in creating or in accessing the database"
-                }
-             ]
-        });
+    function connected(err){
+        if(err) {
+            const error = new Error('Database connection failed');
+            error.status = 500;
+            error.code = 'DB_CONNECTION_ERROR';
+            error.title = 'Database Connection Error';
+            error.detail = 'There was an issue connecting to the database.';
+            return next(error);
+        }
+        console.log('DB created or already existing');
     }
-    console.log('DB created or already existing')
+    
+    let sql = `CREATE TABLE IF NOT EXISTS users(
+        user_id TEXT PRIMARY KEY,
+        username TEXT NOT NULL
+    )`;
+    DB.run(sql,[],(res,err) => {
+        if(err) {
+            const error = new Error('Table creation failed');
+            error.status = 500;
+            error.code = 'table_creation_error';
+            error.title = 'Table Creatiom Error';
+            error.detail = 'There was an issue creating the users table';
+            return next(error);
+        }
+        console.log('users table created')
+    });
+    
+    sql = `CREATE TABLE IF NOT EXISTS exercises(
+        id INTEGER PRIMARY KEY,
+        duration INTEGER NOT NULL,
+        date TEXT NOT NULL,
+        user TEXT,
+        FOREIGN KEY(user) REFERENCES users(user_id)
+    )`;
+    DB.run(sql,[],(err) => {
+        if(err) {
+            const error = new Error('Table creation failed');
+            error.status = 500;
+            error.code = 'table_creation_error';
+            error.title = 'Table Creatiom Error';
+            error.detail = 'There was an issue creating the exercises table';
+            return next(error);
+        }
+        console.log('exercises table created')
+    });
+    return DB;
 };
 
-let sql = `CREATE TABLE IF NOT EXISTS users(
-    user_id TEXT PRIMARY KEY,
-    username TEXT NOT NULL
-)`;
-DB.run(sql,[],(err) => {
-    if(err) {
-        return res.status(500).send({
-            "errors": [
-                {
-                    "status": "500",
-                    "code": "table-creation-error",          
-                    "title": "table creation error",
-                    "detail": "There have been issues in creating the users table"
-                }
-             ]
-        });
-    }
-    console.log('users table created')
-});
-
-sql = `CREATE TABLE IF NOT EXISTS exercises(
-    id INTEGER PRIMARY KEY,
-    duration INTEGER NOT NULL,
-    date TEXT NOT NULL,
-    FOREIGN KEY(user) REFERENCES users(user_id)
-)`;
-DB.run(sql,[],(err) => {
-    if(err) {
-        return res.status(500).send({
-            "errors": [
-                {
-                    "status": "500",
-                    "code": "table-creation-error",          
-                    "title": "table creation error",
-                    "detail": "There have been issues in creating the exercises table"
-                }
-             ]
-        });
-    }
-    console.log('exercises table created')
-});
-
-module.exports = DB;
+module.exports = setupDB;
