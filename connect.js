3a1cf53d7af611cafe51282185084efc38e0271a
Irene Fabbri <irenefabbri.ed@gmail.com>
2025-03-12 18:27:52 +0100

    db creation handled outside

--- connect.js
+++ connect.js
@@ -1,7 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-function setupDB(next) {
-    const DB = new sqlite3.Database('./exercises.db', sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE, connected);
+const DB = new sqlite3.Database('./exercises.db', sqlite3.OPEN_READWRITE, connected);
 
     function connected(err){
         if(err) {
@@ -10,46 +9,9 @@ function setupDB(next) {
             error.code = 'DB_CONNECTION_ERROR';
             error.title = 'Database Connection Error';
             error.detail = 'There was an issue connecting to the database.';
-            return next(error);
+            return;
         }
-        console.log('DB created or already existing');
+        console.log('Connected to the DB');
     }
-    
-    let sql = `CREATE TABLE IF NOT EXISTS users(
-        user_id TEXT PRIMARY KEY,
-        username TEXT NOT NULL
-    )`;
-    DB.run(sql,[],(res,err) => {
-        if(err) {
-            const error = new Error('Table creation failed');
-            error.status = 500;
-            error.code = 'table_creation_error';
-            error.title = 'Table Creatiom Error';
-            error.detail = 'There was an issue creating the users table';
-            return next(error);
-        }
-        console.log('users table created')
-    });
-    
-    sql = `CREATE TABLE IF NOT EXISTS exercises(
-        id INTEGER PRIMARY KEY,
-        duration INTEGER NOT NULL,
-        date TEXT NOT NULL,
-        user TEXT,
-        FOREIGN KEY(user) REFERENCES users(user_id)
-    )`;
-    DB.run(sql,[],(err) => {
-        if(err) {
-            const error = new Error('Table creation failed');
-            error.status = 500;
-            error.code = 'table_creation_error';
-            error.title = 'Table Creatiom Error';
-            error.detail = 'There was an issue creating the exercises table';
-            return next(error);
-        }
-        console.log('exercises table created')
-    });
-    return DB;
-};
 
-module.exports = setupDB;
+module.exports = DB;
