50818dc63003d7f8a86297177843112f9f348507
Irene Fabbri <irenefabbri.ed@gmail.com>
2025-05-06 10:41:47 +0200

    Added infrastructure layer

--- classes.js
+++
@@ -1,7 +0,0 @@
-import { Users } from './Users.js';
-import { Exercises } from './Exercises.js';
-
-export {
-    Users,
-    Exercises
-}
--- server.js
+++
@@ -1,9 +0,0 @@
-import { app } from './app.js';
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-    console.log("Server Listening on PORT:", PORT);
-});
-
-// the listening part is separated so that i can test app.js
